3718c86cb24ad1ec98a0c16a2a1fa0d7819eea8e
Yiwei Ho <davidho0403@gmail.com>
2024-06-06 23:38:00 +0800

    feat: encrypt identity data in db

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Customer extends Model
 {
@@ -15,4 +16,14 @@ class Customer extends Model
         'gender',
         'birthday',
     ];
+
+    public function getIdentityAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
+
+    public function setIdentityAttribute($value)
+    {
+        $this->attributes['identity'] = Crypt::encryptString($value);
+    }
 }
